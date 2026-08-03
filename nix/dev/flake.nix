806d76a94cccba0d7e9438ef08357831feb0986a
Peter A. <ink.splatters@pm.me>
2026-08-03 08:57:11 +0200

    chore: nix/dev/flake.nix:
    - Fix nixpkgs url
    - Add git-hooks.nix binary cache

--- nix/dev/flake.nix
+++ nix/dev/flake.nix
@@ -6,7 +6,16 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://pre-commit-hooks.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+    ];
   };
 
   outputs = _: {};
